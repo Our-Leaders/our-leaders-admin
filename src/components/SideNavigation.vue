0bbaf7d88dc33359d6ab82819c4d3615af3b0322
Ralph Olutola <oreofe.olutola@gmail.com>
2019-12-02 13:39:29 +0100

    Add button for new resource

--- src/components/SideNavigation.vue
+++ src/components/SideNavigation.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="top-0 left-0 bottom-0 right-0 border-gray-c4 border-r w-1/6 pl-8 pr-12">
-
+    <button class="relative border-black border-2 w-full py-2 px-3 flex justify-between items-center">
+      <span>New</span>
+      <span><img src="@/assets/img/add.svg" alt=""></span>
+    </button>
   </div>
 </template>
 
