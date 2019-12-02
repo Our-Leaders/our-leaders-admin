c9352f5437275a28c200355134a13ca732441d1b
Ralph Olutola <oreofe.olutola@gmail.com>
2019-12-02 13:39:01 +0100

    Add side nav component

---
+++ src/components/SideNavigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="top-0 left-0 bottom-0 right-0 border-gray-c4 border-r w-1/6 pl-8 pr-12">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'side-navigation',
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,10 @@ import Vue from 'vue';
 
 import Alert from './Alert.vue';
 import Navigation from './Navigation.vue';
+import SideNavigation from './SideNavigation.vue';
 
 Vue.component('our-alert', Alert);
-// Vue.component('our-navbar', Navigation);
+Vue.component('our-side-navbar', SideNavigation);
 
 export default {
   Navigation,
