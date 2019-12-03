833cde157f8657a276299456ba775985becfb139
Ralph Olutola <oreofe.olutola@gmail.com>
2019-12-03 13:01:21 +0100

    Add country selector component

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 
 import Alert from './Alert.vue';
 import Navigation from './Navigation.vue';
+import CountryList from './CountrySelector.vue';
 import SideNavigation from './SideNavigation.vue';
 
 Vue.component('our-alert', Alert);
+Vue.component('our-country-selector', CountryList);
 Vue.component('our-side-navbar', SideNavigation);
 
 export default {
