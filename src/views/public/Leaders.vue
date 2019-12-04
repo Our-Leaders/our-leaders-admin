1578d628dafb92cf0e2f04e54e10438a3064c632
Ralph Olutola <oreofe.olutola@gmail.com>
2019-12-04 13:18:20 +0100

    Display leader grid on leaders page

--- src/views/public/Leaders.vue
+++ src/views/public/Leaders.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="lg:flex">
-    <div class="lg:w-2/3 xl:w-3/5">
+  <div class="lg:pr-2 xl:pr-0 xl:flex">
+    <div class="w-full xl:w-2/3">
       <header class="flex justify-between items-end">
         <h5 class="text-4xl">
           Leaders (239)
@@ -19,76 +19,16 @@
         </header>
         <p class="font-circular text-gray-c4 text-sm pt-5 pb-5">239 Current Leaders</p>
         <div class="leaders-grid flex flex-wrap">
-          <div class="leader-profile">
-            <img src="" alt="">
+          <div class="leader-profile" v-for="(leader, index) of leaders" :key="index">
+            <img class="w-full mb-3" src="@/assets/img/osibanjo.png" alt="">
             <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
-          </div>
-          <div class="leader-profile">
-            <img src="" alt="">
-            <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
-          </div>
-          <div class="leader-profile">
-            <img src="" alt="">
-            <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
-          </div>
-          <div class="leader-profile">
-            <img src="" alt="">
-            <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
-          </div>
-          <div class="leader-profile">
-            <img src="" alt="">
-            <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
-          </div>
-          <div class="leader-profile">
-            <img src="" alt="">
-            <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
-          </div>
-          <div class="leader-profile">
-            <img src="" alt="">
-            <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
-          </div>
-          <div class="leader-profile">
-            <img src="" alt="">
-            <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
-          </div>
-          <div class="leader-profile">
-            <img src="" alt="">
-            <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
-          </div>
-          <div class="leader-profile">
-            <img src="" alt="">
-            <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
-          </div>
-          <div class="leader-profile">
-            <img src="" alt="">
-            <div class="likes"></div>
-            <p class="name font-circular text-xl font-bold">Muhamadu Buhari</p>
-            <p class="position">President of the Federal Republic of Nigeria</p>
+            <p class="name font-circular text-3xl lg:text-base xl:text-xl font-bold">{{leader.name}}</p>
+            <p class="position text-lg lg:text-sm xl:text-base">{{leader.position}}</p>
           </div>
         </div>
       </div>
     </div>
-    <div class="lg:w-1/3 xl:w-2/5 xl:ml-10">
+    <div class="w-full xl:w-1/3 xl:ml-10">
       aside
     </div>
   </div>
@@ -100,6 +40,48 @@ export default {
   data() {
     return {
       country: 'ngr',
+      leaders: [
+        {
+          name: 'Muhamadu Buhari',
+          position: 'Preseident of the Federal Republic of Nigeria',
+        },
+        {
+          name: 'Muhamadu Buhari',
+          position: 'Preseident of the Federal Republic of Nigeria',
+        },
+        {
+          name: 'Muhamadu Buhari',
+          position: 'Preseident of the Federal Republic of Nigeria',
+        },
+        {
+          name: 'Muhamadu Buhari',
+          position: 'Preseident of the Federal Republic of Nigeria',
+        },
+        {
+          name: 'Muhamadu Buhari',
+          position: 'Preseident of the Federal Republic of Nigeria',
+        },
+        {
+          name: 'Muhamadu Buhari',
+          position: 'Preseident of the Federal Republic of Nigeria',
+        },
+        {
+          name: 'Muhamadu Buhari',
+          position: 'Preseident of the Federal Republic of Nigeria',
+        },
+        {
+          name: 'Muhamadu Buhari',
+          position: 'Preseident of the Federal Republic of Nigeria',
+        },
+        {
+          name: 'Muhamadu Buhari',
+          position: 'Preseident of the Federal Republic of Nigeria',
+        },
+        {
+          name: 'Muhamadu Buhari',
+          position: 'Preseident of the Federal Republic of Nigeria',
+        },
+      ],
     };
   },
   methods: {
@@ -140,16 +122,67 @@ export default {
     }
   }
 
+  .leaders-grid {
+    margin: -1rem;
+
+    @screen sm {
+      margin: -1rem;
+    }
+
+    @screen lg {
+      margin: -0.5rem;
+    }
+
+    @screen xl {
+      margin: -0.7rem;
+    }
+  }
+
   .leader-profile {
-    @apply border border-gray-c4 h-auto mr-2 mb-2;
-    flex: 0 0 24%;
+    flex: 0 0 94%;
+    margin-top: 0.75rem;
+    margin-left: 0.75rem;
+    margin-right: 0.75rem;
+
+    @screen sm {
+      margin-top: 1rem;
+      margin-left: 1rem;
+      margin-right: 1rem;
+      flex: 0 0 45%;
+    }
+
+    @screen lg {
+      margin-top: 0.5rem;
+      margin-left: 0.5rem;
+      margin-right: 0.5rem;
+      flex: 0 0 30%;
+    }
+
+    @screen xl {
+      margin-top: 0.7rem;
+      margin-left: 0.7rem;
+      margin-right: 0.7rem;
+      flex: 0 0 22%;
+    }
+
+    @apply /* border border-gray-c4 */ h-auto mb-12;
 
     .position {
-      max-width: 13rem;
-      width: 100%;
       overflow-x: hidden;
       text-overflow: ellipsis;
       white-space: nowrap;
+
+      @screen sm {
+        max-width: 21rem;
+      }
+
+      @screen lg {
+        max-width: 12rem;
+      }
+
+      @screen xl {
+        max-width: 10.6rem;
+      }
     }
   }
 </style>
