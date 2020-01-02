b52d1b131591126e9fa07f1f3f49b7dadf7ae883
Ralph Olutola <oreofe.olutola@gmail.com>
2020-01-02 22:18:40 +0100

    Add political parties page

--- src/views/index.js
+++ src/views/index.js
@@ -1,11 +1,13 @@
 import Home from './public/Home.vue';
 import SignIn from './auth/SignIn.vue';
 import Leaders from './public/Leaders.vue';
+import Parties from './public/Parties.vue';
 import AdminHome from './public/AdminHome.vue';
 
 export default {
   Home,
   SignIn,
   Leaders,
+  Parties,
   AdminHome,
 };
