084db2505bae99985a162c5c827d4c9e85830678
Ralph Olutola <oreofe.olutola@gmail.com>
2020-01-08 14:34:49 +0100

    Move year and quarter view to their components

---
+++ src/components/politicianDetails/AccomplishmentsViews/QuarterView.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div class="mb-2">
+      <button class="py-1 flex justify-between items-center font-circular text-xs text-gray-96" @click.once="goToYearView">
+        <img src="./../../../assets/img/chevron-left.svg" alt="chevron-left">
+        <span class="ml-1">Back to yearly view</span>
+      </button>
+    </div>
+    <div class="flex justify-between items-center mb-1">
+      <p class="font-circular text-xl font-semibold capitalize">{{quarter}} {{year}}</p>
+      <span class="text-xs font-circular">{{quarters.length}} accomplishment{{quarters.length > 1 ? 's' : ''}}</span>
+    </div>
+    <div v-for="(accomplishment, index) of quarters" :key="index" class="quarter-view-accomplishment border-t border-gray-db relative block">
+      <input type="checkbox" :name="index" :id="index" class="invisible absolute">
+      <label :for="index">
+        <header class="py-3 cursor-pointer">
+          <div class="relative flex justify-between items-center">
+            <span class="font-semibold capitalize font-circular">{{accomplishment.title}}</span>
+            <span class="flex">
+              <span class="text-xs text-gray-96 font-circular mr-2">Published {{accomplishment.createdOn | shortDateFormat}}</span>
+              <img src="./../../../assets/img/chevron-down.svg" alt="chevron-down">
+            </span>
+          </div>
+          <div class="tags mt-2">
+            <div class="inline-block tag text-xs font-circular py-1 px-3 border border-gray-db mr-2 capitalize">
+              tourism
+            </div>
+            <div class="inline-block tag text-xs font-circular py-1 px-3 border border-gray-db mr-2 capitalize">
+              infrastructure
+            </div>
+          </div>
+        </header>
+      </label>
+      <div class="mb-4 pr-12 quarter-view-accomplishment-detail">
+        <!-- image here -->
+        <p class="">{{accomplishment.description}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccomplishmentQuarterView',
+  props: {
+    quarters: {
+      type: Array,
+      required: true,
+    },
+    quarter: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    goToYearView() {
+      this.$emit('onGoToYearView');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .quarter-view-accomplishment-detail {
+    display: none;
+  }
+
+  .quarter-view-accomplishment header img {
+    transition: 0.4s transform;
+  }
+
+  .quarter-view-accomplishment input[type="checkbox"]:checked ~ label header img {
+    transform: rotate(180deg);
+  }
+
+  .quarter-view-accomplishment input[type="checkbox"]:checked ~ .quarter-view-accomplishment-detail {
+    display: block;
+  }
+</style>
--- src/components/politicianDetails/PoliticianAccomplishments.vue
+++ src/components/politicianDetails/PoliticianAccomplishments.vue
@@ -7,81 +7,24 @@
         </div>
       </div>
     </div>
-    <div v-if="view === 'yearly'">
-      <div v-for="(year, index) of sortedYears" :key="index">
-        <p class="font-circular text-xl font-semibold w-full mb-5">{{year}}</p>
-        <div class="quarters">
-          <div class="quarter" v-for="(accomplishments, quarter) of yearlyView[year]" :key="quarter">
-            <div class="py-3 pl-2 pr-3 font-circular border-b border-gray-db flex justify-between items-center leading-none cursor-pointer" @click="goToQuarter(quarter, year)">
-              <span class="font-bold capitalize">{{quarter}}</span>
-              <span class="text-xs text-gray-96 capitalize">{{quarterTags[quarter]}} {{year}}</span>
-            </div>
-            <div
-              class="py-2 px-2 border-gray-db leading-none relative cursor-pointer accomplishment"
-              v-for="(accomplishment, accomplishmentIndex) of filteredAccomplishments(accomplishments)"
-              :key="accomplishmentIndex">
-              <span>{{accomplishment.title}}</span>
-              <div class="absolute top-0 bottom-0 right-0 h-full items-center pr-3 pl-8 accomplishment-action">
-                <button class="text-xs font-circular mr-4">Edit</button>
-                <button class="text-sm"><fa-icon :icon="['fas', 'trash']"></fa-icon></button>
-              </div>
-            </div>
-            <div class="py-3 pl-2 pr-3 font-circular border-gray-db text-xs leading-none cursor-pointer flex justify-between items-center accomplishment" v-if="accomplishments.length > 3">
-              <span>View {{ accomplishments.length - 3 }} more</span>
-              <span><fa-icon :icon="['fas', 'arrow-right']"></fa-icon></span>
-              </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div v-if="view === 'quarter'">
-      <div class="mb-2">
-        <button class="py-1 flex justify-between items-center font-circular text-xs text-gray-96" @click="goToYearView">
-          <img src="./../../assets/img/chevron-left.svg" alt="chevron-left">
-          <span class="ml-1">Back to yearly view</span>
-        </button>
-      </div>
-      <div class="flex justify-between items-center mb-1">
-        <p class="font-circular text-xl font-semibold capitalize">{{selectedQuarter.quarter}} {{selectedQuarter.year}}</p>
-        <span>{{yearlyView[selectedQuarter.year][selectedQuarter.quarter].length}} accomplishment{{yearlyView[selectedQuarter.year][selectedQuarter.quarter].length > 1 ? 's' : ''}}</span>
-      </div>
-      <div v-for="(accomplishment, index) of yearlyView[selectedQuarter.year][selectedQuarter.quarter]" :key="index" class="quarter-view-accomplishment border-t border-gray-db relative block">
-        <input type="checkbox" :name="index" :id="index" class="invisible absolute">
-        <label :for="index">
-          <header class="py-3 cursor-pointer">
-            <div class="relative flex justify-between items-center">
-              <span class="font-semibold capitalize font-circular">{{accomplishment.title}}</span>
-              <span class="flex">
-                <span class="text-xs text-gray-96 font-circular mr-2">Published {{accomplishment.createdOn | shortDateFormat}}</span>
-                <img src="./../../assets/img/chevron-down.svg" alt="chevron-down">
-              </span>
-            </div>
-            <div class="tags mt-2">
-              <div class="inline-block tag text-xs font-circular py-1 px-3 border border-gray-db mr-2 capitalize">
-                tourism
-              </div>
-              <div class="inline-block tag text-xs font-circular py-1 px-3 border border-gray-db mr-2 capitalize">
-                infrastructure
-              </div>
-            </div>
-          </header>
-        </label>
-        <div class="mb-4 pr-12 quarter-view-accomplishment-detail">
-          <!-- image here -->
-          <p class="">{{accomplishment.description}}</p>
-        </div>
-      </div>
-    </div>
+    <component :is="components[view]" v-bind="componentProps" v-on="{ onGoToQuarter: goToQuarter, onGoToYearView: goToYearView }"></component>
   </div>
 </template>
 
 <script>
+import YearlyView from '@/components/politicianDetails/AccomplishmentsViews/YearView.vue';
+import QuarterView from '@/components/politicianDetails/AccomplishmentsViews/QuarterView.vue';
+
 export default {
   name: 'PoliticianAccomplishments',
   props: {
     politician: {
       type: Object,
     },
+  },
+  components: {
+    YearlyView,
+    QuarterView,
   },
   data() {
     return {
@@ -93,17 +36,17 @@ export default {
         q4: 'october - december',
       },
       view: 'yearly',
-      views: ['yearly', 'quarter'],
       selectedQuarter: {
         quarter: '',
         year: '',
       },
+      components: {
+        yearly: YearlyView,
+        quarter: QuarterView,
+      },
     };
   },
   methods: {
-    filteredAccomplishments(accomplishments) {
-      return accomplishments.slice(0, 3);
-    },
     goToQuarter(quarter, year) {
       this.selectedQuarter.quarter = quarter;
       this.selectedQuarter.year = year;
@@ -114,7 +57,7 @@ export default {
     },
   },
   computed: {
-    yearlyView() {
+    accomplishmentByYear() {
       const { accomplishments = [] } = this.politician;
       const stuff1 = {};
       accomplishments.forEach((accomplishment) => {
@@ -135,10 +78,24 @@ export default {
       return stuff1;
     },
     sortedYears() {
-      return Object.keys(this.yearlyView).sort((a, b) => {
+      return Object.keys(this.accomplishmentByYear).sort((a, b) => {
         if (a < b) return 1;
         return -1;
       });
+    },
+    componentProps() {
+      if (this.view === 'yearly') {
+        return {
+          sortedYears: this.sortedYears,
+          accomplishmentByYear: this.accomplishmentByYear,
+        };
+      }
+
+      return {
+        quarter: this.selectedQuarter.quarter,
+        year: this.selectedQuarter.year,
+        quarters: this.accomplishmentByYear[this.selectedQuarter.year][this.selectedQuarter.quarter],
+      };
     },
   },
 };
@@ -152,31 +109,6 @@ export default {
 
       &::-webkit-scrollbar {
         display: none;
-      }
-    }
-  }
-
-  .quarters {
-    @apply flex flex-wrap justify-between;
-
-    .quarter {
-      @apply border border-gray-db h-auto;
-      margin-bottom: 1.25rem;
-      flex: 0 0 calc(50% - 0.625rem);
-
-      .accomplishment {
-        &:not(:last-child) {
-          @apply border-b;
-        }
-
-        &-action {
-          display: none;
-          background: linear-gradient(270deg, #FFFFFF 67.19%, rgba(255, 255, 255, 0) 100%);
-        }
-
-        &:hover .accomplishment-action {
-          display: flex;
-        }
       }
     }
   }
