fee96d3e2689c8aa7095a2538ffa1d98aae4fff5
Abisoye Oke-lawal <abiso.lawal@gmail.com>
2020-05-07 23:37:03 +0100

    - Update Sign In Page

--- src/views/auth/SignIn.vue
+++ src/views/auth/SignIn.vue
@@ -10,7 +10,7 @@
       </div>
     </div>
     <img class="lg:hidden h-16 my-6 mx-12 absolute" src="../../assets/img/logo-b.svg"/>
-    <div class="w-full md:w-full lg:w-2/3 xl:w-2/3 flex flex-col justify-center">
+    <div class="w-full md:w-full lg:w-2/3 xl:w-2/3 flex flex-col justify-center" v-if="page === 0">
       <div class="mx-auto w-6/12">
         <span class="block text-5xl mb-10">Admin Log in</span>
         <form @submit.prevent="proceed">
@@ -27,11 +27,12 @@
           <div class="mb-6">
             <label class="block" for="password">
               <span class="font-semibold font-circular">Password</span>
-              <!-- <span
+              <span
                 class="block cursor-pointer font-regular text-sm
-                text-gray-500 float-right align-middle">
+                text-gray-500 float-right align-middle"
+                @click="page = 1">
                 Forgot Password?
-              </span> -->
+              </span>
             </label>
             <div class="relative">
               <input class="field w-full py-2"
@@ -61,6 +62,90 @@
         </form>
       </div>
     </div>
+    <div class="w-full md:w-full lg:w-2/3 xl:w-2/3 flex flex-col justify-center" v-if="page === 1">
+      <div class="mx-auto w-6/12">
+        <span class="block text-4xl mb-10">Admin Forgot Password Request</span>
+        <form @submit.prevent="requestPasswordReset">
+          <div class="mb-6">
+            <label class="block font-semibold font-circular" for="email">Email</label>
+            <input class="field w-full py-2"
+              type="text"
+              id="email"
+              name="email"
+              v-model="data.email"
+              placeholder="Enter your email"
+              required/>
+          </div>
+          <button
+            :class="{
+              'btn-primary w-full mb-8': true,
+              'loading': loading,
+            }"
+            :disabled="loading">
+            Send Request
+          </button>
+          <button
+            :class="{
+              'w-full mb-8': true,
+            }"
+            @click="page = 0"
+            type="button"
+            :disabled="loading">
+            Cancel
+          </button>
+        </form>
+      </div>
+    </div>
+    <div class="w-full md:w-full lg:w-2/3 xl:w-2/3 flex flex-col justify-center" v-if="page === 2">
+      <div class="mx-auto w-6/12">
+        <span class="block text-4xl mb-10">Admin Reset Password</span>
+        <form @submit.prevent="resetPassword">
+          <div class="mb-6">
+            <label class="block" for="password">
+              <span class="font-semibold font-circular">Password</span>
+            </label>
+            <div class="relative">
+              <input class="field w-full py-2"
+                :type="!displayPassword ? 'password' : 'text'"
+                id="password"
+                name="password"
+                v-model="data.password"
+                placeholder="Enter your password"
+                required>
+              <div class="inline-flex content-center absolute right-0 h-full">
+                <img class="inline-block cursor-pointer m-auto" src="../../assets/img/password-eye.svg" v-if="!displayPassword" @click="togglePassword"/>
+                <img class="inline-block cursor-pointer m-auto"
+                  src="../../assets/img/password-eye-off.svg"
+                  v-if="displayPassword"
+                  @click="togglePassword"/>
+              </div>
+            </div>
+          </div>
+          <div class="mb-6">
+            <label class="block" for="password">
+              <span class="font-semibold font-circular">Confirm Password</span>
+            </label>
+            <div class="relative">
+              <input class="field w-full py-2"
+                type="password"
+                id="password"
+                name="password"
+                v-model="data.confirmPassword"
+                placeholder="Enter your password"
+                required>
+            </div>
+          </div>
+          <button
+            :class="{
+              'btn-primary w-full mb-8': true,
+              'loading': loading,
+            }"
+            :disabled="loading">
+            Set Password
+          </button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -75,12 +160,62 @@ export default {
       data: {
         email: null,
         password: null,
+        confirmPassword: null,
       },
       displayPassword: false,
       loading: false,
+      page: 0,
+      reset: false,
+      token: null,
     };
   },
+  created() {
+    if (this.$route.query.token) {
+      this.page = 2;
+      this.token = this.$route.query.token;
+    }
+  },
   methods: {
+    async requestPasswordReset() {
+      try {
+        this.loading = true;
+        await this.authServices.requestPasswordReset({
+          email: this.data.email.toLowerCase(),
+          isAdmin: true,
+        });
+
+        this.displaySuccess({ message: 'An email has been sent with further instructions.' });
+        this.data.email = null;
+        this.page = 0;
+      } catch (err) {
+        this.displayError(err);
+      } finally {
+        this.loading = false;
+      }
+    },
+    async resetPassword() {
+      try {
+        if (this.data.password !== this.data.confirmPassword) {
+          this.displayError('Password and confirmation password do not match.');
+        } else {
+          this.loading = true;
+          await this.authServices.resetPassword({
+            password: this.data.password,
+            confirmPassword: this.data.confirmPassword,
+            token: this.token,
+          });
+
+          this.displaySuccess({ message: 'Your password has been updated successfully.' });
+          this.data.password = null;
+          this.data.confirmPassword = null;
+          this.page = 0;
+        }
+      } catch (err) {
+        this.displayError(err);
+      } finally {
+        this.loading = false;
+      }
+    },
     async proceed() {
       try {
         this.loading = true;
