c3408797da7ed19530c90637caf1b51a7ba2d1d9
Oreofe Olutola <oreofe.olutola@gmail.com>
2020-05-27 07:18:12 +0100

    Add methods for getting user stats

--- src/services/statisticsServices.js
+++ src/services/statisticsServices.js
@@ -2,8 +2,32 @@ import instance from './services';
 
 const resource = '/statistics';
 
+const parseQueryString = query => Object.keys(query).map(key => `${key}=${query[key]}`).join('&');
+
 export default {
   getStats() {
     return instance.get(resource);
+  },
+
+  getVisits(query = {}) {
+    const queryString = parseQueryString(query);
+    let url = `${resource}/visits`;
+
+    if (queryString) {
+      url = `${url}?${queryString}`;
+    }
+
+    return instance.get(url);
+  },
+
+  getSignups(query = {}) {
+    const queryString = parseQueryString(query);
+    let url = `${resource}/signups`;
+
+    if (queryString) {
+      url = `${url}?${queryString}`;
+    }
+
+    return instance.get(url);
   },
 };
