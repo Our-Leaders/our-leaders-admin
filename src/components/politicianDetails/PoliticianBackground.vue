00ef881caabf8669a272e5fe963e0a5fb787d778
Abisoye Oke-lawal <abiso.lawal@gmail.com>
2020-07-04 00:07:32 +0100

    - Add political background tab mobile

--- src/components/politicianDetails/PoliticianBackground.vue
+++ src/components/politicianDetails/PoliticianBackground.vue
@@ -5,28 +5,28 @@
     </div>
     <div>
       <p class="font-circular text-xl font-semibold w-full mb-5">Personal background</p>
-      <div class="flex mb-3">
-        <div class="w-1/3">Political Party</div>
-        <div class="w-2/3 capitalize" v-if="politician.politicalParty">{{politician.politicalParty.name}} ({{politician.politicalParty.acronym}})</div>
+      <div class="flex flex-wrap mb-3">
+        <div class="w-full lg:w-1/3 font-bold lg:font-normal">Political Party</div>
+        <div class="w-full lg:w-2/3 capitalize" v-if="politician.politicalParty">{{politician.politicalParty.name}} ({{politician.politicalParty.acronym}})</div>
       </div>
-      <div class="flex mb-3">
-        <div class="w-1/3">DOB/Age</div>
-        <div class="w-2/3">{{politician.dob | dateFormat('-')}} / {{politician.dob | age}} years</div>
+      <div class="flex flex-wrap mb-3">
+        <div class="w-full lg:w-1/3 font-bold lg:font-normal">DOB/Age</div>
+        <div class="w-full lg:w-2/3">{{politician.dob | dateFormat('-')}} / {{politician.dob | age}} years</div>
       </div>
       <!-- <div class="flex mb-3">
         <div class="w-1/3">Country</div>
         <div class="w-2/3 capitalize">{{countries[politician.country].name}}</div>
       </div> -->
-      <div class="flex mb-3">
-        <div class="w-1/3">State of origin</div>
-        <div class="w-2/3 capitalize">{{politician.stateOfOrigin}}</div>
+      <div class="flex flex-wrap mb-3">
+        <div class="w-full lg:w-1/3 font-bold lg:font-normal">State of origin</div>
+        <div class="w-full lg:w-2/3 capitalize">{{politician.stateOfOrigin}}</div>
       </div>
     </div>
     <div class="mt-8">
       <p class="font-circular text-xl font-semibold w-full mb-5">Political background</p>
-      <div class="flex mb-3" v-for="(politicalBackground, index) of politician.politicalBackground" :key="index">
-        <div class="w-1/3">{{politicalBackground.position}}</div>
-        <div class="w-2/3">
+      <div class="flex flex-wrap mb-3" v-for="(politicalBackground, index) of politician.politicalBackground" :key="index">
+        <div class="w-full lg:w-1/3 font-bold lg:font-normal">{{politicalBackground.position}}</div>
+        <div class="w-full lg:w-2/3">
           <div>{{politicalBackground.description}}</div>
           <div>
             <span v-if="politicalBackground.startDate">{{politicalBackground.startDate | shortDateFormat}} - </span>
@@ -40,9 +40,9 @@
     </div>
     <div class="mt-8">
       <p class="font-circular text-xl font-semibold w-full mb-5">Educational background</p>
-      <div class="flex mb-3" v-for="(educationalBackground, index) of politician.educationalBackground" :key="index">
-        <div class="w-1/3 capitalize">{{educationalBackground.degree}}</div>
-        <div class="w-2/3 capitalize">
+      <div class="flex flex-wrap mb-3" v-for="(educationalBackground, index) of politician.educationalBackground" :key="index">
+        <div class="w-full lg:w-1/3 font-bold lg:font-normal capitalize">{{educationalBackground.degree}}</div>
+        <div class="w-full lg:w-2/3 capitalize">
           <!-- University of Lagos. MBA Marketing, 1993 -->
           <span class="capitalize">{{educationalBackground.institution}}</span> <span v-if="educationalBackground.graduationYear">{{educationalBackground.graduationYear}}</span>
         </div>
@@ -54,14 +54,14 @@
     <div class="mt-8">
       <p class="font-circular text-xl font-semibold w-full mb-5">Professional background</p>
       <div class="mb-3" v-for="(professionalBackground, index) of politician.professionalBackground" :key="index">
-        <div class="flex">
-          <div class="w-1/3">Title</div><div class="w-2/3">{{professionalBackground.title}}</div>
+        <div class="flex flex-wrap">
+          <div class="w-full lg:w-1/3 font-bold lg:font-normal">Title</div><div class="w-full lg:w-2/3 mb-5 lg:mb-0">{{professionalBackground.title}}</div>
         </div>
-        <div class="flex">
-          <div class="w-1/3">Description</div><div class="w-2/3">{{professionalBackground.description}}</div>
+        <div class="flex flex-wrap">
+          <div class="w-full lg:w-1/3 font-bold lg:font-normal">Description</div><div class="w-full lg:w-2/3 mb-5 lg:mb-0">{{professionalBackground.description}}</div>
         </div>
-        <div class="flex">
-          <div class="w-1/3">Duration</div><div class="w-2/3">
+        <div class="flex flex-wrap">
+          <div class="w-full lg:w-1/3 font-bold lg:font-normal">Duration</div><div class="w-full lg:w-2/3 mb-5 lg:mb-0">
             <span v-if="professionalBackground.startYear">{{professionalBackground.startYear}} - </span>
             <span v-if="professionalBackground.endYear">{{professionalBackground.endYear}}</span>
           </div>
