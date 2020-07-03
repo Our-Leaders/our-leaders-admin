437bd16c07f49c57774da24177dc48397e10ff06
Abisoye Oke-lawal <abiso.lawal@gmail.com>
2020-07-03 21:02:07 +0100

    - Mobile view for political party detail page

--- src/views/public/PartyDetails.vue
+++ src/views/public/PartyDetails.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="lg:flex">
-    <div class="xl:w-2/3 relative">
-      <button class="border-black border py-1 px-3 flex justify-between items-center font-circular absolute top-0 right-0" @click="editProfile">Edit Party</button>
+    <div class="w-full px-4 pt-8 lg:px-0 lg:pt-0 xl:w-2/3 relative">
+      <div class="lg:hidden flex flex-wrap mb-4 lg:mb-0">
+        <p class="text-4xl leading-tight">{{party.name}} ({{party.acronym}})</p>
+      </div>
+      <button class="border-gray-500 border py-2 px-4 flex justify-between text-sm items-center font-circular mb-4 lg:mb-0 lg:absolute lg:top-0 lg:right-0" @click="editProfile">Edit Profile</button>
       <header ref="imageHere">
-        <div class="politician-image h-32 w-40 bg-gray-96" :style="{ 'background-image': 'url('+ partyLogo +')'  }"></div>
+        <div class="politician-image h-64 lg:h-32 w-full lg:w-40 bg-gray-96" :style="{ 'background-image': 'url('+ partyLogo +')'  }"></div>
       </header>
-      <header class="w-full h-auto bg-white z-50" ref="stickyHeader" style="padding-top: 2.5rem;">
+      <header class="w-full h-auto bg-white z-50 pt-4 lg:pt-20" ref="stickyHeader">
         <div class="mb-12" v-if="!show">
-          <p class="text-5xl leading-tight">{{party.name}} ({{party.acronym}})</p>
-          <div class="inline-flex mb-3 items-center">
+          <p class="hidden lg:block text-5xl leading-tight">{{party.name}} ({{party.acronym}})</p>
+          <div class="flex lg:hidden">
+            <div class="w-1/2">
+              <div class="inline-flex likes font-circular text-base text-gray-96">
+                <span class="flex mr-3 items-baseline cursor-pointer">
+                  <img class="mr-2 w-5 relative" src="@/assets/img/upvote.svg" alt="upvote">
+                  {{party.votes.up | numberFormat}}
+                </span>
+                <span class="flex items-baseline cursor-pointer">
+                  <img class="mr-2 w-5 relative downvote" src="@/assets/img/downvote.svg" alt="downvote">
+                  {{party.votes.down | numberFormat}}
+                </span>
+              </div>
+            </div>
+            <div class="w-1/2 pt-1">
+              <a class="ml-6 float-right inline-block" href="#"><img class="h-4" src="@/assets/img/social/instagram.svg" alt="instagram link"></a>
+              <a class="ml-6 float-right inline-block" href="#"><img class="h-4" src="@/assets/img/social/twitter.svg" alt="twitter link"></a>
+              <a class="ml-6 float-right inline-block" href="#"><img class="h-4" src="@/assets/img/social/facebook.svg" alt="facebook link"></a>
+            </div>
+          </div>
+          <div class="hidden lg:inline-flex mb-3 items-center">
             <p class="font-circular text-xs">Share party</p>
             <a class="ml-6" href="#"><img class="h-4" src="@/assets/img/social/facebook.svg" alt="facebook link"></a>
             <a class="ml-6" href="#"><img class="h-4" src="@/assets/img/social/twitter.svg" alt="twitter link"></a>
@@ -111,7 +133,7 @@ export default {
   }
 
   .downvote {
-    top: 12px;
+    top: 8px;
 
     @screen lg {
       top: 5px;
